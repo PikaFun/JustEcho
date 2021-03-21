771a00de5588b1510606dce654ee04c32165c93b
PikaFun <32321499+PikaFun@users.noreply.github.com>
2021-03-22 01:38:31 +0300

    bot.py
    echo bot

---
+++ bot.py
@@ -0,0 +1,15 @@
+#предварительно инсталлим: pip install aiogram.py
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+bot = Bot(token='токен сюды')
+disp = Dispatcher(bot)
+
+@disp.message_handler()
+async def main(message: types.Message):
+    await bot.send_message(message.from_user.id, message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(disp)
